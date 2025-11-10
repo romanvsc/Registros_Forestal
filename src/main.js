78795786b7fab9fbdecd62567e3eee89d8379343
RomanVC <romanvscprogramming@gmail.com>
2025-11-10 20:58:08 -0300

    Fix service worker registration path for correct relative access

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import './assets/main.css'
 // Registrar Service Worker
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
+    navigator.serviceWorker.register('./service-worker.js')
       .then(registration => {
         console.log('Service Worker registrado:', registration)
       })
